8d1b4b6486dee4eeb1c4a92fd2227c859de8e0a5
Kautilya Reddy Chappidi <kautilya97@gmail.com>
2023-11-25 16:32:38 +0000

    add new packages for services, parameters and msgs

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -1,41 +1,80 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// // Copyright 2016 Open Source Robotics Foundation, Inc.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
 
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+// #include <rclcpp/rclcpp.hpp>
+// #include <std_msgs/msg/string.hpp>
 
-class MinimalPublisher : public rclcpp::Node {
- public:
-  MinimalPublisher() : Node("minimal_publisher") {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
+// class MinimalPublisher : public rclcpp::Node {
+//  public:
+//   MinimalPublisher() : Node("minimal_publisher") {
+//     publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
 
-    auto publish_message = [this]() -> void {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, Turtlesquad!";
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    };
+//     auto publish_message = [this]() -> void {
+//       auto message = std_msgs::msg::String();
+//       message.data = "Hello, Turtlesquad!";
+//       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+//       publisher_->publish(message);
+//     };
 
-    timer_ = this->create_wall_timer(std::chrono::seconds(1), publish_message);
+//     timer_ = this->create_wall_timer(std::chrono::seconds(1), publish_message);
+//   }
+
+//  private:
+//   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+//   rclcpp::TimerBase::SharedPtr timer_;
+// };
+
+// int main(int argc, char *argv[]) {
+//   rclcpp::init(argc, argv);
+//   rclcpp::spin(std::make_shared<MinimalPublisher>());
+//   rclcpp::shutdown();
+//   return 0;
+// }
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tutorial_interfaces/msg/num.hpp"                                            // CHANGE
+
+using namespace std::chrono_literals;
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("minimal_publisher"), count_(0)
+  {
+    publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);  // CHANGE
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
- private:
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+private:
+  void timer_callback()
+  {
+    auto message = tutorial_interfaces::msg::Num();                                   // CHANGE
+    message.num = this->count_++;                                                     // CHANGE
+    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.num << "'");    // CHANGE
+    publisher_->publish(message);
+  }
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_;             // CHANGE
+  size_t count_;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
--- cpp_pubsub/src/subscriber_member_function.cpp
+++ cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,34 +1,67 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// // Copyright 2016 Open Source Robotics Foundation, Inc.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
 
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+// #include <rclcpp/rclcpp.hpp>
+// #include <std_msgs/msg/string.hpp>
 
-class MinimalSubscriber : public rclcpp::Node {
- public:
-  MinimalSubscriber() : Node("minimal_subscriber") {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "chatter", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
-          RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
-        });
+// class MinimalSubscriber : public rclcpp::Node {
+//  public:
+//   MinimalSubscriber() : Node("minimal_subscriber") {
+//     subscription_ = this->create_subscription<std_msgs::msg::String>(
+//         "chatter", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
+//           RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
+//         });
+//   }
+
+//  private:
+//   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+// };
+
+// int main(int argc, char *argv[]) {
+//   rclcpp::init(argc, argv);
+//   rclcpp::spin(std::make_shared<MinimalSubscriber>());
+//   rclcpp::shutdown();
+//   return 0;
+// }
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tutorial_interfaces/msg/num.hpp"                                       // CHANGE
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+  : Node("minimal_subscriber")
+  {
+    subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>(    // CHANGE
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
- private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+private:
+  void topic_callback(const tutorial_interfaces::msg::Num & msg) const  // CHANGE
+  {
+    RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg.num << "'");     // CHANGE
+  }
+  rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_;  // CHANGE
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
   rclcpp::shutdown();
